d1d05614978b4bfd75107684d66eed8191dd77d1
son loku <jacobrborgen@gmail.com>
2020-03-14 21:46:27 -0500

    added jaws db connection

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,18 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
+if (process.env.JAWSDB_URL) {
+  var connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "ilbid64=",
+  database: "burger_db"
+});
+};
+
 var connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
